9f3bdcd944646761d4aed556f17f536de7bfb13e
sarthak-pokharel <sarthak.whenever@gmail.com>
2022-11-11 21:40:31 +0545

    non disabling stream button for reloading purpose

--- client/room/room.js
+++ client/room/room.js
@@ -27,53 +27,53 @@ async function playSongFromUrl(surl){
 
 let queue = [], currentSong = -1;
 
-function setEvents() {
-    createUserForm.addEventListener('submit', async ev => {
-        ev.preventDefault();
-        let songUrl = createUserForm['song-url'].value;
-        await playSongFromUrl(songUrl);
+async function addtoqueue(e){
+    e.preventDefault();
+    loadingDisableables.disable();
+    let surl = createUserForm['song-url'].value;
+    let songDetails = await getSource(surl);
+    queue.push({surl,songDetails});
+    let qlc = queue.length-1;
+    $("#queue-display tbody").append(`
+
+<tr>
+<td style="cursor: pointer;"> ▶️ </td>
+<td>${songDetails.full.videoDetails.title.substr(0,40).padEnd(43,".")}</td>
+<td style="cursor: pointer;"> ❌ </td>
+</tr>
+    `);
+    $("tr").last().children().eq(0).on('click', function(){
+        let thisid = [...this.parentNode.parentNode.children].indexOf(this.parentNode);
+        currentSong = thisid;
+        pl[0].src="";
+        loadingDisableables.disable();
+        playSongFromQueue(currentSong);
     });
+    $("tr").last().children().eq(2).on('click', function(){
+        let thisid = [...this.parentNode.parentNode.children].indexOf(this.parentNode);
+        console.log(thisid);
+        queue.splice(thisid, 1);
+        $(this).parent().remove();
+        if(thisid == currentSong){
+            playNext();
+        }
+    });
+    createUserForm['song-url'].value = "";
+    if(queue.length == 1){
+        currentSong = 0;
+        playSongFromQueue(currentSong);
+    }
+}
+
+function setEvents() {
+    createUserForm.addEventListener('submit', addtoqueue);
     pl[0].onseeked = ()=>{
         syncronizationUpdater();
     }
     pl[0].onended = ()=>{
         playNext();
     }
-    $("#add-to-queue").on('click',async function(){
-        let surl = createUserForm['song-url'].value;
-        let songDetails = await getSource(surl);
-        queue.push({surl,songDetails});
-        let qlc = queue.length-1;
-        $("#queue-display tbody").append(`
-
-<tr>
-    <td style="cursor: pointer;"> ▶️ </td>
-    <td>${songDetails.full.videoDetails.title.substr(0,40).padEnd(43,".")}</td>
-    <td style="cursor: pointer;"> ❌ </td>
-</tr>
-        `);
-        $("tr").last().children().eq(0).on('click', function(){
-            let thisid = [...this.parentNode.parentNode.children].indexOf(this.parentNode);
-            currentSong = thisid;
-            pl[0].src="";
-            loadingDisableables.disable();
-            playSongFromQueue(currentSong);
-        });
-        $("tr").last().children().eq(2).on('click', function(){
-            let thisid = [...this.parentNode.parentNode.children].indexOf(this.parentNode);
-            console.log(thisid);
-            queue.splice(thisid, 1);
-            $(this).parent().remove();
-            if(thisid == currentSong){
-                playNext();
-            }
-        });
-        createUserForm['song-url'].value = "";
-        if(queue.length == 1){
-            currentSong = 0;
-            playSongFromQueue(currentSong);
-        }
-    })
+    $("#add-to-queue").on('click',addtoqueue);
 }
 
 function playNext(){
